6d1114572cad1013cef083fc59556a694cc519d3
Everton Martins <evertonmartins154@gmail.com>
2024-03-26 11:31:54 -0300

    Notificacao Add domain classes

---
+++ notificacao/src/main/java/com/martinseverton/notificacao/domain/Proposta.java
@@ -0,0 +1,25 @@
+package com.martinseverton.notificacao.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class Proposta {
+
+    private Long id;
+
+    private BigDecimal valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovada;
+
+    private boolean integrada;
+
+    private String observacao;
+
+    private Usuario usuario;
+}
---
+++ notificacao/src/main/java/com/martinseverton/notificacao/domain/Usuario.java
@@ -0,0 +1,24 @@
+package com.martinseverton.notificacao.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class Usuario {
+
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private BigDecimal renda;
+}
