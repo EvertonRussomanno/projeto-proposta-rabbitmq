0990d00eefe15fa7d67d6db830617f4164056bd4
Everton Martins <evertonmartins154@gmail.com>
2024-03-26 12:47:16 -0300

    Notificacao add AmazonSNS configuration and NotificacaoSnsService

--- notificacao/src/main/java/com/martinseverton/notificacao/listener/PropostaPendenteListener.java
+++ notificacao/src/main/java/com/martinseverton/notificacao/listener/PropostaPendenteListener.java
@@ -2,14 +2,20 @@ package com.martinseverton.notificacao.listener;
 
 import com.martinseverton.notificacao.constante.MensagemConstante;
 import com.martinseverton.notificacao.domain.Proposta;
+import com.martinseverton.notificacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
+    @Autowired
+    private NotificacaoSnsService notificacaoSnsService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
     public void propostaPendente(Proposta proposta){
         String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+        notificacaoSnsService.notificar(proposta.getUsuario().getTelefone(), mensagem);
     }
 }
