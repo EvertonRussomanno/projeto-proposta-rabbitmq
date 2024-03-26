2c074463316d0c4a3517542c80b2a418f5210876
Everton Martins <evertonmartins154@gmail.com>
2024-03-26 15:11:58 -0300

    AnaliseCredito add domain package, applicatio.properties and rabbitmqListener

---
+++ analisecredito/src/main/java/com/martinseverton/analisecredito/domain/Usuario.java
@@ -0,0 +1,23 @@
+package com.martinseverton.analisecredito.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class Usuario {
+
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private BigDecimal renda;
+}
---
+++ analisecredito/src/main/java/com/martinseverton/analisecredito/listener/PropostaEmAnaliseListener.java
@@ -0,0 +1,14 @@
+package com.martinseverton.analisecredito.listener;
+
+import com.martinseverton.analisecredito.domain.Proposta;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaEmAnaliseListener {
+
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaEmAnalise(Proposta proposta){
+        
+    }
+}
